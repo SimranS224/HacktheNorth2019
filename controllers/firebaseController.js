491561583056b21f0ac7d2ec00e093b8d0661d79
brian1999lin <brian1999lin@gmail.com>
2019-09-14 17:13:37 -0400

    finish endpoints

--- controllers/firebaseController.js
+++ controllers/firebaseController.js
@@ -23,7 +23,7 @@ function createUser(req, res) {
 		action: ''
 	};
 	// get a new key for POST
-	const newPostKey = firebase.database().ref().child('posts').push().key;
+	const newPostKey = firebase.database().ref().child('users').push().key;
 
 	// create list of items to update
 	let updates = {};
@@ -35,10 +35,56 @@ function createUser(req, res) {
   		})
   		.catch((err) => {
   			console.log(`ERR: Creation of user with username ${req.params.username} failed with error + ${err}`);
+  			res.status(500).send();
   		});
+}
+
+async function incrementScore(req, res) {
+	const userId = req.params.userId;
+	console.log('Incrementing score for user with ID#' + userId);
+	let initialData;	// stores initial data prior to incrementing of score
+	await firebase.database().ref('/users/' + userId).once('value')
+		.then((snapshot) => {
+	  		initialData = snapshot.val()
+		})
+		.catch((err) => {
+			console.log(`ERR: Fetching user with ID#${userId} failed with error ${err}`);
+			res.status(500).send();
+		});
+
+	// create list of items to update
+	let updates = {};
+	updates['/users/' + userId] = {...initialData, score: initialData.score + 1};
+	return firebase.database().ref().update(updates)
+		.then(() => {
+			res.status(200).send();
+		})
+		.catch((err) => {
+			console.log(`ERR: Creation of user with username ${req.params.username} failed with error + ${err}`);
+  			res.status(500).send();
+		});
+}
+
+function updateUser(req, res) {
+	const userId = req.params.userId;
+	console.log('Updating user with ID#' + userId);
+	const updateBody = req.body;
+	// create list of items to update
+	let updates = {};
+	updates['/users/' + userId] = updateBody;
+	return firebase.database().ref().update(updates)
+		.then(() => {
+			res.status(200).send();
+		})
+		.catch((err) => {
+			console.log(`ERR: Creation of user with username ${req.params.username} failed with error + ${err}`);
+  			res.status(500).send();
+		});
 }
 
 module.exports = {
 	getUser: getUser,
-	createUser: createUser
+	createUser: createUser,
+	incrementScore: incrementScore,
+	updateUser: updateUser
 };
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const fs = require('fs');
 const nconf = require('nconf');
 nconf.file({file:'./config/config.json'});
 const firebaseController = require('./controllers/firebaseController');
+
+// use bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,6 +17,10 @@ app.get('/', (req, res) => {
 app.get('/user/:userId', firebaseController.getUser);
 
 app.post('/user/:username', firebaseController.createUser);
+
+app.patch('/user/:userId', firebaseController.incrementScore);
+
+app.put('/user/:userId', firebaseController.updateUser);
 
 app.listen(8000, () => {
   console.log('Hack the North 2019 app listening on port 8000!')	
