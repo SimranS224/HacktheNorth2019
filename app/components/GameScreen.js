d065ad2741ca3b62b5c5d9b729448cd643f1beea
Wenqin Ye <wenqinye@v1044-wn-rt-b-141-91.campus-dynamic.uwaterloo.ca>
2019-09-14 16:14:28 -0400

    init

---
+++ app/components/GameScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div>
+            Game
+        </div>
+      );
+    }
+  }
+  
+  export default Game;
