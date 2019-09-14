dc203313c3aeeea4af2d5f4e2b0349774c9323b4
Simran <snike2610@gmail.com>
2019-09-14 17:55:53 -0400

    current progress

---
+++ uploadImage.js
@@ -0,0 +1,26 @@
+const util = require('util');
+const fs = require('fs');
+
+const PredictionApiClient = require("@azure/cognitiveservices-customvision-prediction");
+const setTimeoutPromise = util.promisify(setTimeout);
+const predictionKey = "<your prediction key>";
+const predictionResourceId = "<your prediction resource id>";
+const sampleDataRoot = "<path to image files>";
+
+const endPoint = "https://southcentralus.api.cognitive.microsoft.com"
+
+const publishIterationName = "classifyModel";
+
+(async () => {
+    console.log("Creating project...");
+    const predictor = new PredictionApiClient(predictionKey, endPoint);
+    const testFile = fs.readFileSync(`${sampleDataRoot}/Test/test_image.jpg`);
+
+    const results = await predictor.classifyImage(sampleProject.id, publishIterationName, testFile);
+
+    // Step 6. Show results
+    console.log("Results:");
+    results.predictions.forEach(predictedResult => {
+        console.log(`\t ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}%`);
+    });
+})()
