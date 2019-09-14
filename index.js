0bd56e394bb5c14c0bf49bb44b8778056e01c12e
brian1999lin <brian1999lin@gmail.com>
2019-09-14 12:54:07 -0400

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(8000, () => {
+  console.log('Hack the North 2019 app listening on port 8000!')
+});
