7983147a8cadae0927d63a80c8a5e4d67f2d7627
Simran <snike2610@gmail.com>
2019-09-14 21:10:37 -0400

    cleaning up

--- sign-recognition/uploadImage.js
+++ sign-recognition/uploadImage.js
@@ -23,33 +23,8 @@ const projectId = "79ced5f2-36ce-48e2-911f-cb4ed1619d88";
 
     // Step 6. Show results
     console.log("Results:");
+    const signs = {}
     results.predictions.forEach(predictedResult => {
         console.log(`\t ${predictedResult.tagName}: ${(predictedResult.probability * 100.0).toFixed(2)}%`);
     });
 })()
-
-// $.ajax({
-//     type: "POST",
-//     url: url,
-//     data: imageBytes,
-//     processData: false,
-//     headers: {
-//         "Prediction-Key": "2d4ae585659b490dae3b3a53bf022562",
-//         "Content-Type": "multipart/form-data"
-//     }
-// }).done(function (data) {
-//     var predictions = data.predictions;
-//     var artists = [predictions.find(o => o.tagName === 'Picasso'), predictions.find(o => o.tagName === 'Rembrandt'), predictions.find(o => o.tagName === 'Pollock')];
-//     var sortedArtists = _.sortBy(artists, 'probability').reverse();
-//     var possibleArtist = sortedArtists[0];
-
-//     if (possibleArtist.probability > 0.9) {
-//         $('#analysisResults').html('<div class="matchLabel">' + possibleArtist.tagName + ' (' + (possibleArtist.probability * 100).toFixed(0) + '%)' + '</div>');
-//     }
-//     else {
-//         $('#analysisResults').html('<div class="noMatchLabel">Unknown artist</div>');
-//     }
-
-// }).fail(function (xhr, status, err) {
-//     alert(err);
-// });
